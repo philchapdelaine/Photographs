383b378306469ddf9e1ca375b600d1bfb46f5eca
Philippe Chapdelaine <pchapy@gmail.com>
2022-08-23 15:57:48 -0700

    deleted template, started list

---
+++ src/components/content.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+const Content = () => {
+    return(
+        <div className="content-container">
+            <ul className="collection_nav">
+                <li className="collection_nav_item">001</li>
+                <li className="collection_nav_item">002</li>
+                <li className="collection_nav_item">003</li>
+                <li className="collection_nav_item">004</li>
+                <li className="collection_nav_item">005</li>
+            </ul>
+        </div>
+
+    );
+}
+
+export default Content
--- src/components/layout.js
+++ src/components/layout.js
@@ -13,44 +13,13 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+    <div>
+      {children}
       </div>
-    </>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
